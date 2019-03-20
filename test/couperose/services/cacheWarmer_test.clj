d5044ca8f6e239072f7a23f91f7484a7540e8abb
Ivan Golikov <igolikov@octoberry.ru>
2019-03-20 20:11:51 +0300

    Added lexems test

--- test/couperose/services/cacheWarmer_test.clj
+++ test/couperose/services/cacheWarmer_test.clj
@@ -13,3 +13,8 @@
                                           "https://test.com/v1/groups" (fn [request] {:status 200 :headers {} :body body})}
                          (let [groups (future (Thread/sleep 1000) (c/get warmer/groupsUrl))]
                            (is (= body (:body @groups))))))))
+
+(deftest getBaseLexemsTest
+  (testing "should return first word"
+    (let [word (get (warmer/getBaseLexems "words/words") 0)]
+      (is (= "Hello" word)))))
